ef32d22b81da38c7af13369052d1295c792bbf00
sivasaikum <sivasaikumarallampati@gmail.com>
2025-02-06 11:50:43 +0530

    expense project script

---
+++ mysql.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+
+USERID=$( id -u )
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FLODER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FLODER/$LOG_FILE-$TIMESTAMP"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOT() {
+   if [ $USERID -ne 0 ]
+    then
+    echo "ERROR :: you must have a root access to execute this script"
+    exit 1
+    fi 
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+dnf list installed mysql-server &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y &>>$LOG_FILE_NAME
+    VALIDATE $? "mysql installing"
+    
+else
+    echo -e "MYSQL is already ... $G INSTALLED $N "
+fi
+
+systemctl enable mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling mysqld server"
+
+systemctl start mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "Starting mysqld server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE_NAME
+VALIDATE $? "Setting Root Password"
